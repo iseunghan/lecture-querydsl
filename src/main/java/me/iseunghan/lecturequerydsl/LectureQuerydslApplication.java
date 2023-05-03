85aee7d223790ea4ba7a8deff2918763eef6a773
iseunghan <tmdgks301@naver.com>
2023-05-03 21:59:13 +0900

    init

---
+++ src/main/java/me/iseunghan/lecturequerydsl/LectureQuerydslApplication.java
@@ -0,0 +1,13 @@
+package me.iseunghan.lecturequerydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureQuerydslApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LectureQuerydslApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/iseunghan/lecturequerydsl/LectureQuerydslApplicationTests.java
@@ -0,0 +1,13 @@
+package me.iseunghan.lecturequerydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureQuerydslApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
