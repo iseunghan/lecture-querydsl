f69b1470b0b61a20070c1e118e508b005ac90a75
iseunghan <tmdgks301@naver.com>
2023-06-01 23:02:11 +0900

    순수 JPARepository Builder -> WhereParams로 변경
    - 재사용성과 확장에 더 강하고, 컴포지션 하기 좋음

--- src/main/java/me/iseunghan/lecturequerydsl/repository/MemberJpaRepository.java
+++ src/main/java/me/iseunghan/lecturequerydsl/repository/MemberJpaRepository.java
@@ -1,14 +1,22 @@
 package me.iseunghan.lecturequerydsl.repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import me.iseunghan.lecturequerydsl.dto.MemberSearchCond;
+import me.iseunghan.lecturequerydsl.dto.MemberTeamDto;
+import me.iseunghan.lecturequerydsl.dto.QMemberTeamDto;
 import me.iseunghan.lecturequerydsl.entity.Member;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import static me.iseunghan.lecturequerydsl.entity.QMember.member;
+import static me.iseunghan.lecturequerydsl.entity.QTeam.team;
+import static org.springframework.util.StringUtils.hasText;
 
 @Repository
 public class MemberJpaRepository {
@@ -60,6 +68,77 @@ public class MemberJpaRepository {
         return queryFactory.selectFrom(member)
                 .where(member.username.eq(username))
                 .fetch();
+    }
+
+    public List<MemberTeamDto> dynamicSearchMember_Builder(MemberSearchCond cond) {
+        BooleanBuilder builder = new BooleanBuilder();  // 주의! 기본 조건(동적쿼리로 인해 결과 개수가 천차만별이므로)으로 페이징을 꼭 넣어줘야 한다.
+
+        if (hasText(cond.getUsername())) {
+            builder.and(member.username.eq(cond.getUsername()));
+        }
+        if (hasText(cond.getTeamName())) {
+            builder.and(team.name.eq(cond.getTeamName()));
+        }
+        if (Objects.nonNull(cond.getAgeGoe())) {
+            builder.and(member.age.goe(cond.getAgeGoe()));
+        }
+        if (Objects.nonNull(cond.getAgeLoe())) {
+            builder.and(member.age.loe(cond.getAgeLoe()));
+        }
+
+        return queryFactory.select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")
+                ))
+                .from(member)
+                .join(member.team, team)
+                .where(builder)
+                .fetch();
+    }
+
+    public List<MemberTeamDto> dynamicSearchMember_WhereParams(MemberSearchCond cond) {
+        return queryFactory.select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")
+                ))
+                .from(member)
+                .join(member.team, team)
+                .where(
+                        usernameEq(cond.getUsername()),
+                        teamNameEq(cond.getTeamName()),
+                        memberAgeGoe(cond.getAgeGoe()),
+                        memberAgeLoe(cond.getAgeLoe())
+                )
+                .fetch();
+    }
+
+    /**
+     * Predicate말고 BooleanExpression 타입으로 반환하는 이유는, and(), or() 메소드를 제공해줘서 composition하기 좋음!
+     */
+    private BooleanExpression usernameEq(String username) {
+        return hasText(username) ?
+                member.username.eq(username) : null;
+    }
+
+    private BooleanExpression teamNameEq(String teamName) {
+        return hasText(teamName) ?
+                team.name.eq(teamName) : null;
+    }
+
+    private BooleanExpression memberAgeGoe(Integer memberAge) {
+        return Objects.nonNull(memberAge) ?
+                member.age.goe(memberAge) : null;
+    }
+
+    private BooleanExpression memberAgeLoe(Integer memberAge) {
+        return Objects.nonNull(memberAge) ?
+                member.age.loe(memberAge) : null;
     }
 
 }
--- src/test/java/me/iseunghan/lecturequerydsl/repository/MemberJpaRepositoryTest.java
+++ src/test/java/me/iseunghan/lecturequerydsl/repository/MemberJpaRepositoryTest.java
@@ -1,7 +1,10 @@
 package me.iseunghan.lecturequerydsl.repository;
 
 import jakarta.persistence.EntityManager;
+import me.iseunghan.lecturequerydsl.dto.MemberSearchCond;
+import me.iseunghan.lecturequerydsl.dto.MemberTeamDto;
 import me.iseunghan.lecturequerydsl.entity.Member;
+import me.iseunghan.lecturequerydsl.entity.Team;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -59,6 +62,69 @@ class MemberJpaRepositoryTest {
         // findAll
         List<Member> member2 = memberJpaRepository.findAll_dsl();
         assertThat(member1).containsExactly(member);
+    }
+
+    void setup() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 10, teamA);
+        Member member3 = new Member("member3", 10, teamA);
+        Member member4 = new Member("member4", 10, teamB);
+        Member member5 = new Member("member5", 50, teamB);
+        Member member6 = new Member("member6", 60, teamB);
+        Member member7 = new Member("member7", 70, teamB);
+        Member member8 = new Member("member8", 80, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+        em.persist(member5);
+        em.persist(member6);
+        em.persist(member7);
+        em.persist(member8);
+
+        em.flush();
+        em.clear();
+    }
+
+    @Test
+    void dynamicQuerydsl_builder_Test() {
+        // given
+        setup();
+
+        MemberSearchCond cond = new MemberSearchCond();
+        cond.setUsername("member5");
+        cond.setAgeGoe(40);
+        cond.setAgeLoe(80);
+        cond.setTeamName("teamB");
+
+        // when
+        List<MemberTeamDto> result = memberJpaRepository.dynamicSearchMember_Builder(cond);
+
+        // then
+        assertThat(result).extracting("username").containsExactly("member5");
+    }
+
+    @Test
+    void dynamicQuerydsl_WhereParams_Test() {
+        // given
+        setup();
+
+        MemberSearchCond cond = new MemberSearchCond();
+        cond.setUsername("member5");
+        cond.setAgeGoe(40);
+        cond.setAgeLoe(80);
+        cond.setTeamName("teamB");
+
+        // when
+        List<MemberTeamDto> result = memberJpaRepository.dynamicSearchMember_WhereParams(cond);
+
+        // then
+        assertThat(result).extracting("username").containsExactly("member5");
     }
 
 }
